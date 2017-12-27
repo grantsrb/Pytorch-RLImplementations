b66ef348b25bc90aa998baf21bb8d5c48aeddacb
Satchel Grant <grantsrb@gmail.com>
2017-12-26 16:09:47 -0800

    add modular files from ppo

---
+++ deep_qlearning/fitter.py
@@ -0,0 +1,128 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+import numpy as np
+import os
+import utils
+from utils import discount, gae
+import model
+
+if torch.cuda.is_available():
+    torch.FloatTensor = torch.cuda.FloatTensor
+
+class Fitter():
+    def __init__(self, net, optimizer, pool, val_const=.5, entropy_const=0.01, spatio_const=0.001, gamma=0.99, lambda_=0.97, predict_spatio=False):
+        self.obs_space = net.obs_space
+        self.act_space = net.act_space
+        self.net = net
+        self.optimizer = optimizer
+        self.pool = pool
+        self.val_const = val_const
+        self.entropy_const = entropy_const
+        self.gamma = gamma
+        self.lambda_ = lambda_
+        self.spatio_const = spatio_const
+        self.predict_spatio = predict_spatio
+
+        #self.old_datas = [[]]*n_olddatas
+        #self.n_olddatas = n_olddatas
+        #self.data_idx = 0
+
+
+    def fit(self, new_data, epochs=10, batch_size=128, clip_const=0.2, max_norm=0.5):
+        """
+        Runs stochastic gradient descent on the collected data.
+
+        new_data: a sequence of data lists in the following order
+            actions, observations, discounted rewards, pi values, advantages
+        """
+
+        """
+        Each array of data comes in with a shape of [step, env]. To get each
+        env's step to line up in a single long array, we need to transpose each array
+        and then ravel them.
+        """
+        actions, observs, rewards, values, deltas, dones = new_data
+
+        async_arr = [self.pool.apply_async(discount, [rewards[:,i], self.gamma, dones[:,i]]) for i in range(rewards.shape[1])]
+        rewards = [async.get() for async in async_arr]
+        rewards = Variable(torch.FloatTensor(rewards).view(-1))
+        actions = torch.LongTensor(actions).permute(1,0).contiguous().view(-1)
+        observs = observs.swapaxes(0,1).reshape((-1,)+self.obs_space)
+        observs = Variable(torch.from_numpy(observs).float())
+
+        old_vals, old_pis, spatios, spatio_preds = self.net.forward(observs)
+        old_vals = Variable(old_vals.data)
+        old_pis = self.net.softmax(old_pis)
+        old_pis = old_pis.data[torch.arange(0,old_pis.size(0)).long(),actions]
+
+        async_arr = [self.pool.apply_async(discount, [deltas[:,i], self.gamma*self.lambda_, dones[:,i]]) for i in range(deltas.shape[1])]
+        advantages = [async.get() for async in async_arr]
+        advantages = Variable(torch.FloatTensor(advantages).view(-1))
+
+        returns = Variable(advantages.data + old_vals.data.squeeze())
+        #advantages = Variable((advantages-torch.mean(advantages))/(torch.std(advantages)+1e-7))
+
+        old_pis = torch.clamp(old_pis, 1e-7, 1)
+        old_pis = Variable(old_pis)
+
+        n_data_pts = len(actions)
+        n_loops = n_data_pts//batch_size
+        avg_loss = 0
+        avg_clip = 0
+        avg_val = 0
+        avg_entropy = 0
+        for epoch in range(epochs):
+            indices = torch.randperm(n_data_pts)
+            running_loss,running_clip,running_val,running_entropy = 0,0,0,0
+
+            for i in range(n_loops):
+                self.optimizer.zero_grad()
+                idxs = indices[i*batch_size:(i+1)*batch_size]
+                batch_acts = actions[idxs]
+                batch_obs = observs[idxs]
+                batch_advs = advantages[idxs]
+                batch_returns = returns[idxs]
+                batch_old_pis = old_pis[idxs]
+                batch_old_vals = old_vals[idxs]
+                batch_rewards = rewards[idxs]
+
+                values, pi_raw, spatios, spatio_preds = self.net.forward(batch_obs)
+
+                probs = self.net.softmax(pi_raw)
+                new_pis = probs[torch.arange(0,probs.size()[0]).long(), batch_acts]
+                ratios = new_pis/batch_old_pis
+                clipped_ratios = torch.clamp(ratios, 1.0-clip_const, 1.0+clip_const)
+                clip_loss = -torch.mean(torch.min(ratios*batch_advs, clipped_ratios*batch_advs))
+
+                vals_clipped = batch_old_vals + torch.clamp(values.squeeze()-batch_old_vals, -clip_const, clip_const)
+                v1 = (values.squeeze() - batch_rewards)**2
+                v2 = (vals_clipped - batch_rewards)**2
+                val_loss = self.val_const * .5 * torch.mean(torch.max(v1, v2))
+
+                logprobs = self.net.logsoftmax(pi_raw)
+                entropy_loss = -self.entropy_const * torch.mean(probs*logprobs)
+
+                loss = clip_loss + val_loss - entropy_loss
+
+                running_loss += loss.data[0]
+                running_clip += clip_loss.data[0]
+                running_val += val_loss.data[0]
+                running_entropy += entropy_loss.data[0]
+
+                loss.backward()
+                norm = nn.utils.clip_grad_norm(self.net.parameters(), max_norm)
+                self.optimizer.step()
+
+            self.net.check_grads()
+            avg_loss += running_loss/n_data_pts
+            avg_clip += running_clip/n_data_pts
+            avg_val += running_val/n_data_pts
+            avg_entropy += running_entropy/n_data_pts
+
+        return avg_loss/epochs, avg_clip/epochs, avg_val/epochs, avg_entropy/epochs, 0
+
+        # Update data
+        # self.old_datas[self.data_idx] = new_data
+        # self.data_idx = (self.data_idx + 1) % self.n_olddatas
---
+++ deep_qlearning/runner.py
@@ -0,0 +1,115 @@
+import torch
+from torch.autograd import Variable
+import numpy as np
+import spatiotemp_model as model
+import utils
+from multiprocessing import Pool
+import gym
+
+class Runner():
+    """
+    Runner is used to collect a rollout of data from the environment. Inspired by OpenAI's baselines.
+    """
+
+    def __init__(self, env_name, n_envs, pool):
+        self.n_envs = n_envs
+        self.envs = [gym.make(env_name) for i in range(n_envs)]
+        for i in range(n_envs):
+            self.envs[i].seed(i)
+        self.obs_bookmarks = [self.envs[i].reset() for i in range(n_envs)]
+        obs_shape = utils.preprocess(self.obs_bookmarks[0]).shape
+        self.prev_bookmarks = [np.zeros(obs_shape).astype(np.float32) for i in range(n_envs)]
+        self.obs_shape = (obs_shape[0]*2,)+obs_shape[1:]
+        self.pool = pool
+        self.avg_reward = 0
+        self.running_reward = 0
+        self.reward_count = 0
+        self.episode = 0
+        self.T = 0 # Tracks total environmental steps taken
+
+
+
+    def rollout(self, net, n_tsteps, batch_norm, gamma=0.99, render=False):
+        ep_actions, ep_observs, ep_rewards, ep_values, ep_deltas, ep_dones = [], [], [], [], [], []
+        first_roll = True
+        self.episode += 1
+        for i in range(n_tsteps):
+            self.T+=self.n_envs # Tracks total number of frames
+            if render: self.envs[0].render()
+
+            # Prep observation
+            async_arr = [self.pool.apply_async(utils.prep_obs, [obs, prev]) for obs, prev in zip(self.obs_bookmarks, self.prev_bookmarks)]
+            results = [async.get() for async in async_arr]
+            prepped_obses, prev_bookmarks = zip(*results)
+            self.prev_bookmarks = np.asarray(prev_bookmarks)
+            ep_observs.append(prepped_obses)
+
+            # Take action
+            if torch.cuda.is_available():
+                t_obses = Variable(torch.from_numpy(np.asarray(prepped_obses)).float().cuda())
+            else:
+                t_obses = Variable(torch.from_numpy(np.asarray(prepped_obses)).float())
+            values, raw_outputs, spatios, spatio_preds = net.forward(t_obses)
+            action_preds = net.softmax(raw_outputs).data.tolist()
+            async_arr = [self.pool.apply_async(utils.get_action, [pred, net.act_space]) for pred in action_preds] # Sample action from policy's distribution
+            actions = [async.get() for async in async_arr]
+            ep_actions.append(actions)
+
+            # Add 2 to actions for possible actions 2 or 3 (pong specific)
+            results = [self.envs[i].step(action+2) for i,action in enumerate(actions)]
+            obs_bookmarks, rewards, dones, infos = zip(*results)
+            self.obs_bookmarks = np.asarray(obs_bookmarks)
+            dones = np.asarray(dones)
+            rewards = np.asarray(rewards)
+
+            # Track average reward
+            r_indices = (rewards!=0)
+            if self.reward_count < 100:
+                self.running_reward += np.sum(rewards[r_indices])
+                self.reward_count += np.sum(r_indices)
+                if self.reward_count > 0:
+                    self.avg_reward = self.running_reward/float(self.reward_count)
+            else:
+                if np.sum(r_indices) > 0:
+                    self.avg_reward = .99*self.avg_reward + .01*np.mean(rewards[r_indices])
+
+            # Collect deltas for GAE later where delta = r(t) + gamma*V(t+1) - V(t)
+            values = values.data.squeeze().cpu().numpy()
+            if not first_roll:
+                ep_deltas.append(ep_rewards[-1]+gamma*values-ep_values[-1])
+            else:
+                first_roll = False
+            ep_values.append(values)
+            ep_rewards.append(rewards)
+            ep_dones.append(dones)
+
+            # Reset environments that are finished
+            for i,d in enumerate(dones):
+                if d:
+                    self.obs_bookmarks[i] = self.envs[i].reset()
+                    self.prev_bookmarks[i] = np.zeros_like(self.prev_bookmarks[i])
+
+        # End of rollout bootstrapping
+        ep_dones[-1][:] = 1. # Track the end of the rollout
+        bootstrap_indices = (ep_rewards[-1] == 0) # Pong specific
+        ep_rewards[-1][bootstrap_indices] = ep_values[-1][bootstrap_indices] # Bootstrap
+        ep_deltas.append(ep_rewards[-1]-ep_values[-1])
+
+
+        ep_observs = np.asarray(ep_observs, dtype=np.float32)
+        ep_rewards = np.asarray(ep_rewards, dtype=np.float32)
+        ep_values = np.asarray(ep_values, dtype=np.float32)
+        ep_deltas = np.asarray(ep_deltas, dtype=np.float32)
+        ep_dones = np.asarray(ep_dones, dtype=np.int)
+
+        return ep_actions, ep_observs, ep_rewards, ep_values, ep_deltas, ep_dones
+
+
+
+
+
+
+
+
+
+#
